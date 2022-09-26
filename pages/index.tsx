29b5fe8e52318508a1761b5fb1c86b41c9bbe559
ZAIIIKVAR <89867276+ZAIIIKVAR@users.noreply.github.com>
2022-09-26 11:45:30 +0300

    modified:   pages/index.tsx
    	modified:   components/index.tsx
    
    	components/Rating/

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home(): JSX.Element{
   return (
     <div>
       <Htag tag='h1'>text</Htag>
-      <Button apperance="primary" arrow="right">Кнопка</Button>
+      <Button apperance="primary" arrow="right"  >Кнопка</Button>
       <Button apperance="ghost" arrow="down">Кнопка2</Button>
       <P size='s'>FDS</P>
       <P size='m'>FDS</P>
