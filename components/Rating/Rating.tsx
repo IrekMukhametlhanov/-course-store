38350a3691e3c218765583829dcf83ed8cfa2665
ZAIIIKVAR <89867276+ZAIIIKVAR@users.noreply.github.com>
2022-09-26 11:46:16 +0300

    new file:   components/Rating/Rating.module.css 	new file:   components/Rating/Rating.props.ts 	new file:   components/Rating/Rating.tsx 	new file:   components/Rating/star.svg      modified:   components/index.tsx

---
+++ components/Rating/Rating.tsx
@@ -0,0 +1,33 @@
+import { Ratingprops } from "./Rating.props";
+import styles from './Rating.module.css';
+import cn from 'classnames';
+import StarIcon from './star.svg';
+import { useEffect, useState } from "react";
+
+
+export const Rating = ({isEditable = false, rating, setRating,...props}:Ratingprops): JSX.Element =>{
+  const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
+
+  useEffect(() =>{
+      constructRating(rating);
+  },[rating])
+
+  const constructRating = (currentRating: number) =>{
+    const updateArray = ratingArray.map((r: JSX.Element, i: number)=>{
+      return(
+        <StarIcon
+         className = {cn(styles.star,{
+          [styles.fill]: i< currentRating
+         })}
+        
+        />
+      );
+    });
+    setRatingArray(updateArray);
+  };
+    return(
+      <div  {...props}>
+        {ratingArray.map((r,i) => (<span key={i}>{r}</span>))}
+      </div>  
+    );
+}
