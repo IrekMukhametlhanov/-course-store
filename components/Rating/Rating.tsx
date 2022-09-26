c883a5031bd2ee3e1e61d839b8caa0766eb677b4
ZAIIIKVAR <89867276+ZAIIIKVAR@users.noreply.github.com>
2022-09-26 14:13:43 +0300

    modified:   components/Rating/Rating.module.css 	modified:   components/Rating/Rating.tsx

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -15,18 +15,21 @@ export const Rating = ({ isEditable = false, rating, setRating, ...props }: Rati
   const constructRating = (currentRating: number) => {
     const updateArray = ratingArray.map((r: JSX.Element, i: number) => {
       return (
+        <span
+        className={cn(styles.star, {
+          [styles.filled]: i < currentRating,
+          [styles.editable]: isEditable,
+        })}
+        onMouseEnter={() => changeDisplay(i + 1)}
+        onMouseLeave={() => changeDisplay(rating)}
+        onClick={() => onClick(i + 1)}
+        >
         <StarIcon
-          className={cn(styles.star, {
-            [styles.filled]: i < currentRating,
-            [styles.editable]: isEditable,
-          })}
-          onMouseEnter={() => changeDisplay(i + 1)}
-          onMouseLeave={() => changeDisplay(rating)}
-          onClick={() => onClick(i + 1)}
           tabIndex={isEditable ? 0: -1}
           onKeyDown={(e: KeyboardEvent<SVGAElement>)=>isEditable && handleSpace(i+1, e)}
 
         />
+        </span>
       );
     });
     setRatingArray(updateArray);
